20c8be6a40aebf891c26d8db16c924766a934b8b
Diya Shrestha <diyashrestha2000@gmail.com>
2023-03-09 22:06:50 +0545

    input component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import InputField from "./components/InputField";
+import {useState} from "react";
+
+// import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState({username: '', password: ''})
+    const {username, password} = data
+    const [display, setDisplay]=useState({})
+    const changeHandler=e=>{
+        setData({...data,[e.target.name]:[e.target.value]})
+    }
+    const submitHandler=e=>{
+        e.preventDefault();
+        console.log(data)
+     setDisplay(data)
+    }
+
+
+    return (
+        <form >
+            <InputField type='username' name='username' placeholder='Username'  value={username} onChange={changeHandler}></InputField>
+            <InputField type='password' name='password' placeholder='Password' value={password}  onChange={changeHandler}></InputField>
+            <span>{display.username}</span>
+
+            <button onClick={submitHandler}>submit</button>
+        </form>
+    );
 }
 
 export default App;
